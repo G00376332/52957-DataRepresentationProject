c8072fbb53ceaa1af8ba83153f4ac57fa800b96f
unknown <g00376332@gmit.ie>
2020-11-20 18:36:07 +0000

    Creating database for my project

---
+++ data.sql
@@ -0,0 +1,38 @@
+--show databases;
+
+create database baltic;
+use baltic;
+
+--show tables;
+
+create table harvest (
+id int NOT NULL,
+employeeName VARCHAR (250),
+fieldSection VARCHAR (250),
+variety VARCHAR (250),
+quantity INT,
+PRIMARY KEY(id));
+
+insert into harvest (id, employeeName, fieldSection, variety, quantity) values
+('100','Jan Kowalski', 'A1', 'Patriot', '300'),
+('101','Roman Malinowski', 'A3', 'Duke', '150'),
+('105','Zbyszek Dzik', 'C2', 'Toro', '420'),
+('107','Ola Miodek', 'B3', 'Brigitta', '180'),
+('120','Maja Nowak', 'C1', 'Bluecrop', '260'),
+('135','Beata Cichy', 'B2', 'Nelson', '200');
+
+--select * from harvest;
+
+create table users (
+id int NOT NULL auto_increment,
+username varchar(250),
+password varchar(250),
+PRIMARY KEY(id));
+
+--select * from users;
+
+
+insert into users (username, password) values ("admin", "password");
+
+
+--select * from users where username = 'admin' AND password='password';
