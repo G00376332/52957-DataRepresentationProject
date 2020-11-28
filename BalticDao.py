24034cdd4601a3dba2c37b6679c83853cd708265
unknown <g00376332@gmit.ie>
2020-11-28 15:51:30 +0000

    Update templates file and python

--- BalticDao.py
+++ BalticDao.py
@@ -28,3 +28,78 @@ class BalticDao:
         self.db.commit()
         cursor.close()
         return cursor.lastrowid
+   def getAll(self):
+        cursor = self.db.cursor()
+        sql = 'select * from harvest'
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        returnArray = []
+        for result in results:
+            resultAsDict = self.convertToDict(result)
+            returnArray.append(resultAsDict)
+
+        cursor.close()
+        return returnArray
+
+    def findById(self, id):
+        cursor = self.db.cursor()
+        sql = 'select * from harvest where id = %s'
+        values = [id]
+        cursor.execute(sql, values)
+        result = cursor.fetchone()
+        cursor.close()
+        return self.convertToDict(result)
+
+    def update(self, harvest):
+        cursor = self.db.cursor()
+        sql = "update harvest set employeeName = %s, fieldSection = %s, variety = %s, quantity = %s where id = %s"
+        values = [
+            harvest['employeeName'], harvest['fieldSection'], harvest['variety'], harvest['quantity'],harvest['id']
+        ]
+        cursor.execute(sql, values)
+        self.db.commit()
+        cursor.close()
+        return harvest
+
+    def delete(self, id):
+        cursor = self.db.cursor()
+        sql = 'delete from harvest where id = %s'
+        values = [id]
+        cursor.execute(sql, values)
+        self.db.commit()
+        cursor.close()
+        return {}
+
+    def convertToDict(self, result):
+        colnames = ['id', 'employeeName', 'fieldSection', 'variety', 'quantity']
+        harvest = {}
+
+        if result:
+            for i, colName in enumerate(colnames):
+                value = result[i]
+                harvest[colName] = value
+        return harvest
+		
+    def checkUser(self, username, password):
+        cursor = self.db.cursor()
+        sql = "select * from users where username = %s and password = %s"
+        values = [username, password]
+        cursor.execute(sql, values)
+        # Fetch one record and return result
+        result = cursor.fetchone()
+        cursor.close()
+        return self.convertToDict2(result)
+
+    def convertToDict2(self, result):
+        colnames = ["id", "username", "password"]
+        #print(colnames)
+        item = {}
+
+        if result:
+            for i, colName in enumerate(colnames):
+                #print(colnames)
+                value = result[i]
+                item[colName] = value
+        return item
+
+balticDao = BalticDao()
--- server.py
+++ server.py
@@ -7,3 +7,40 @@ from BalticDao import balticDao
 app = Flask(__name__,
             static_url_path='',
             static_folder='templates')
+
+@app.route('/')
+def home():
+    if not 'username' in session:
+        return redirect(url_for('login'))
+
+    return redirect(url_for('harvestData'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['uname'] != '' or request.form['psw'] != '':
+            # Checking username and password in  DAO
+            username = request.form['uname']
+            password = request.form['psw']
+            #For development process only! 
+            #print("Username:" +username, "Password:" +password)
+            foundUser = balticDao.checkUser(username, password)
+            # if the user is not in the database, throw the error
+            if not foundUser:
+                error = 'Invalid Credentials. Please try again.'
+            # if the user exists move to harvest data   
+            else:
+                session['username'] = username
+                return redirect(url_for('harvestData'))
+        else:
+            print(error)
+            return error
+    return render_template('index.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)  
+    return redirect(url_for('login'))
+
+@app.route('/home')
